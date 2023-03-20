90801228ff613e55b5d732ea87cc636d713602e0
EnrikeKiker <kickercertsoft@outlook.com>
2023-03-20 16:31:52 -0600

    Refactorizacion de repositorio, añadio metodos y modifico la presentación

--- js/HU-03.js
+++ js/HU-03.js
@@ -1,8 +1,4 @@
 /*Proceso de comparación de resultados*/
-/*Vista(Presentación)*/
-class VistaRondaResultado{
-
-}
 
 /*Controlador(Presentación)*/
 class ControladorRondaResultado{
@@ -10,23 +6,59 @@ class ControladorRondaResultado{
         this.servicioJugador = servicioJugador;
     }
 //Realiza el proceso de calcular elección inyectando la dependencia el servicio para su referencia
-    calcularEleccion(ServicioJugador){
-        
-    }
-    calcularEleccion(){
+    iniciaCalcularEleccion(){
         let respuesta = this.servicioJugador.calcularEleccion();
         if(respuesta == true){
             this.verificarUltimaRonda();
         }
         else{
             console.log('Error: No se pudo calcular la elección');
+            //llamada a vista
         }
+    }
+    calcularEleccion(){
+
     }
 //Comprueba la ronda final para en vez de cambiar a la vista RondaEleccion pase a la vista RondaResultado
     verificarUltimaRonda(){
-
+        let ronda = 1;
+        
+        return true;
+    }
+//temporizador de 3 segundos
+    tiempoRestante(){
+        setTimeout(this.calcularTiempoRestante,1);
     }
 
+    calcularTiempoRestante(){
+        let segundos, repetir;
+        let actualizaTiempo = document.getElementById('actualizaTiempo');
+        console.log(actualizaTiempo);
+        function iniciarTiempo(){
+            ponerTiempo();
+            conteoRegresivo();
+        }
+        function ponerTiempo(){
+            segundos = 3;
+        }
+        function conteoRegresivo(){
+            repetir = setInterval(calcularTiempo,1000);
+        }
+        function calcularTiempo(){
+            if(segundos > 0){
+                console.log(segundos);
+                --segundos;
+                actualizaTiempo.innerHTML = `0:0${segundos}`;
+            }
+            else{
+                //Me lleva a la vista RondaEleccion o vista de Partida Finalizada
+                clearInterval(repetir);
+                console.log('Cambia a otra vista');
+                alert('Cambia de vista');
+            }
+        }
+        iniciarTiempo();
+    }
 }
 
 /*Servicio(Negocio)*/ 
@@ -36,49 +68,38 @@ class ServicioJugador{
     }
 //Crea la elecciòn de la máquina
     crearEleccionMaquina(){
-        let palabra = ['piedra','papel','tijera'];
-        let eleccionMaquina = palabra[Math.floor(Math.random()*palabra.length)];
+        let opcion = ['piedra','papel','tijera'];
+        let eleccionMaquina = opcion[Math.floor(Math.random()*opcion.length)];
         console.log('Maquina: '+eleccionMaquina);
         return eleccionMaquina;
     }
 //Método extra para crear la elección del jugador y poder probar comparar elecciones
     crearEleccionJugador(){
-        let palabra = ['piedra','papel','tijera'];
-        let eleccionJugador = palabra[Math.floor(Math.random()*palabra.length)];
+        let opcion = ['piedra','papel','tijera'];
+        let eleccionJugador = opcion[Math.floor(Math.random()*opcion.length)];
         console.log('Jugador: '+eleccionJugador);
         return eleccionJugador;
     }
 //Compara las elecciones del jugador y de la máquina
     compararElecciones(eleccionJugador,eleccionMaquina){
-        let resultado = 'perdio';
+        let resultado = 'Perdiste';
         if(
             (eleccionJugador == 'piedra' && eleccionMaquina == 'tijera')||
             (eleccionJugador == 'papel' && eleccionMaquina == 'piedra')||
             (eleccionJugador == 'tijera' && eleccionMaquina == 'papel')
         ){
-            resultado = 'gano';
+            resultado = 'Ganaste';
         }
-        console.log(resultado);
         return resultado;
     }
 //Otorga la puntuación correspondiente si gano(1 punto) o perdio(0 puntos) el jugador
     otorgarPuntuacion(resultado){
         let puntos = 0;
-        if(resultado == 'gano'){
+        if(resultado == 'Ganaste'){
             puntos = 1;
         }
         console.log('Puntos que obtuvo: '+puntos);
         return puntos;
-    }
-    setPuntos(puntos){
-        let sumaPuntos = [];
-        sumaPuntos = puntos;
-        jugador6.setPuntos(puntos);
-        return jugador6;
-    }
-//Guardar la puntuación en el repositorio
-    actualizarPuntuacion(){
-        repositorioObjetos.actualizarBD();
     }
 }
 
@@ -111,9 +132,9 @@ class Jugador{
     return this.puntos;
     }
 
-    setEleccion(jugador) 
+    setEleccion(eleccion) 
     {
-    this.eleccion = jugador.elecion;
+    this.eleccion = eleccion;
     }
 
     getEleccion()
@@ -125,105 +146,110 @@ class Jugador{
 /*Repositorio*/
 class RepositorioJugador{
     constructor(){
-        this.jugador = [];
+        this.arregloJugador = [];
+        this.id=0;
     }
 //Create
-    crearJugador(Jugador){
-        
-        for (let i = 0; i < this.jugador.length; i++) {
-            if (this.jugador[i].nombre == Jugador.nombre) {
+    crearJugador(Jugador){//Recibe el objeto jugador
+        this.arregloJugador = JSON.parse(sessionStorage.getItem('DatosJugadores')) || [];//Comprueba que no este vacío y obtiene el sessionStorage
+        for (let i = 0; i < this.arregloJugador.length; i++) {
+            if (this.arregloJugador[i].nombre == Jugador.nombre) {
                 console.log('El jugador: ' + Jugador.nombre + ' ya existe');
                 return false;
             }
         }
-        this.jugador.push(Jugador);
+        Jugador.id = this.id++;
+        this.arregloJugador.push(Jugador);
+        sessionStorage.setItem('DatosJugadores',JSON.stringify(this.arregloJugador));//Guarda en el sessionStorage
         console.log('Se agrego usuario: '+Jugador.nombre);
         return true;
     }
-    almacenarJugadores(){
-        sessionStorage.setItem('jugador',JSON.stringify(this.jugador));
-    }
 //Retrieve
-    recuperarJugador(Jugador){
-        for(let i=0;i<this.jugador.length;i++){
-            if(this.jugador[i].nombre==Jugador.nombre){
-            console.log('Se encontro '+Jugador.nombre);
-                return Jugador.nombre;
+    recuperarJugador(IDJugador){//recibe el id del jugador
+        this.arregloJugador = JSON.parse(sessionStorage.getItem('DatosJugadores'));
+        for(let i = 0 ; i < this.arregloJugador.length ; i++){
+            if(this.arregloJugador[i].id==IDJugador){
+                console.log('Se recupero: '+this.arregloJugador[i].nombre+' con ID: '+this.arregloJugador[i].id);
+                let tipoJugador = new Jugador();//Asignamos el tipo Jugador al objeto recuperado
+                tipoJugador.id = this.arregloJugador[i].id;
+                tipoJugador.nombre = this.arregloJugador[i].nombre;
+                tipoJugador.puntos = this.arregloJugador[i].puntos;
+                tipoJugador.eleccion = this.arregloJugador[i].eleccion;
+                return tipoJugador;//Regresa el objeto tipo Jugador
             }
         }
-        console.log('No se encuentra '+Jugador.nombre+' dentro de la base de datos');
-    }
-    obtenerJugadores(){
-        let obtener = JSON.parse(sessionStorage.getItem('jugador'));
-        console.log(obtener);
-        return obtener;
+        console.log('No se pudo recuperar al jugador: '+IDJugador.nombre+' con ID: '+IDJugador.id);
+        return false;
     }
 //Update
-    actualizarJugador(Jugador){
-        for(let i=0;i<this.jugador.length;i++){
-            if(this.jugador[i].nombre==Jugador.nombre){
-                this.jugador[i]=Jugador;
+    actualizarJugador(Jugador){//Recibe objeto Jugador
+        this.arregloJugador = JSON.parse(sessionStorage.getItem('DatosJugadores'));
+        this.arregloJugador.pop();
+        for (let i = 0; i < this.arregloJugador.length; i++) {
+            if (this.arregloJugador[i].nombre == Jugador.nombre) {
+                console.log('El jugador: ' + Jugador.nombre + ' no se pudo actualizar');
                 return false;
             }
         }
-        console.log('No se puede actualizar, ya que no existe');
-    }
-    actualizarBD(){
-        sessionStorage.setItem('jugador',JSON.stringify(this.jugador));
-        console.log('Se actualizo la BD');
+        this.arregloJugador.push(Jugador);
+        sessionStorage.setItem('DatosJugadores',JSON.stringify(this.arregloJugador));
+        console.log('Se actualizo el jugador: '+Jugador.nombre);
+        return true;
     }
 //Delete
-    eliminarJugador(Jugador){
-        for(let i=0;i<this.jugador.length;i++){
-            if(this.jugador[i]==Jugador.nombre){
-                delete this.jugador[i];
+    eliminarJugador(IDJugador){//Elimina objeto Jugador
+        this.arregloJugador = JSON.parse(sessionStorage.getItem('DatosJugadores'));
+        for(let i=0;i<this.arregloJugador.length;i++){
+            if(this.arregloJugador[i].id==IDJugador.id){
+                this.arregloJugador.pop(this.arregloJugador[i]);
                 return false;
             }
         }
         console.log('No existe el elemento para eliminar');
     }
-    removerJugadores(){
-        sessionStorage.removeItem('jugador');
-    }
 //Mostrar los jugadores del arreglo
     mostrarJugadores(){
-        console.log(this.jugador);
+        console.log('Desde el Array:');
+        console.log(this.arregloJugador);
+        console.log('Desde el sessionStorage: '+sessionStorage.getItem('DatosJugadores'))
     }
 }
 //Punto de inicio de HU-03
 //Inicializar repositorio con 5 jugadores
 let repositorioObjetos = new RepositorioJugador();
+
 let jugador0 = new Jugador ('ElMasPerron',3,'papel');
 let jugador1 = new Jugador ('CacahuetesConSalsa',2,'piedra');
 let jugador2 = new Jugador ('TangaDPerro',2,'piedra');
 let jugador3 = new Jugador ('TamalDVerde',2,'piedra');
 let jugador4 = new Jugador ('Animalote',2,'piedra');
-let jugador5 = new Jugador ('ElMasPerron',3,'papel');
-let jugador6 = new Jugador ('ReKga2');
-
+let jugador5 = new Jugador ('ReKga2');
 repositorioObjetos.crearJugador(jugador0);
 repositorioObjetos.crearJugador(jugador1);
 repositorioObjetos.crearJugador(jugador2);
 repositorioObjetos.crearJugador(jugador3);
 repositorioObjetos.crearJugador(jugador4);
-repositorioObjetos.crearJugador(jugador5);
-repositorioObjetos.crearJugador(jugador6);
 repositorioObjetos.crearJugador(jugador4);
-repositorioObjetos.almacenarJugadores();
-repositorioObjetos.recuperarJugador(jugador2);
-repositorioObjetos.recuperarJugador(jugador1);
-let obtener = repositorioObjetos.obtenerJugadores();
-console.log(obtener);
-repositorioObjetos.mostrarJugadores();
+repositorioObjetos.crearJugador(jugador5);
 
 let servicio1 = new ServicioJugador();
 let eleccionJugador = servicio1.crearEleccionJugador();
+let imagenJugador = document.getElementById('imagenJugador');
+imagenJugador.innerHTML = `<img src="imagenes/${eleccionJugador}F.jpg" alt="">`;
 let eleccionMaquina = servicio1.crearEleccionMaquina();
+let imagenMaquina = document.getElementById('imagenMaquina');
+imagenMaquina.innerHTML = `<img src="imagenes/${eleccionMaquina}F.jpg" alt="">`;
 let resultado = servicio1.compararElecciones(eleccionJugador,eleccionMaquina);
+console.log(resultado);
+let resultadoRonda = document.getElementById('resultadoTexto');
+resultadoRonda.innerHTML = resultado;
 let puntos = servicio1.otorgarPuntuacion(resultado);
-jugador6.setPuntos(puntos);
-servicio1.actualizarPuntuacion();
+let jugadorRecuperado = repositorioObjetos.recuperarJugador(5);
+jugadorRecuperado.setPuntos(puntos);
+jugadorRecuperado.setEleccion(eleccionJugador);
+console.log(jugadorRecuperado);
+repositorioObjetos.actualizarJugador(jugadorRecuperado);
 
-
-
-
+//Proceso del controlador
+let controlador1 = new ControladorRondaResultado();
+controlador1.tiempoRestante();
